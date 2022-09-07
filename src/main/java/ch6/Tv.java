b42ed985b51f85442f6b0130605bbe52a44c445e
안경만 <rudaks012@naver.com>
2022-09-08 08:45:24 +0900

    Feat : 자바의 정석 6단원 객체지향 프로그래밍

---
+++ src/main/java/ch6/Tv.java
@@ -0,0 +1,20 @@
+package ch6;
+
+public class Tv {
+
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+
+    void channelUp() {
+        ++channel;
+    }
+
+    void channelDown() {
+        --channel;
+    }
+}
