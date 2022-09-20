b1e657c1c0c555d14fce58c48b41697ed4efee20
gyeongman <rudaks012@naver.com>
2022-09-20 23:41:50 +0900

    Feat : 메서드 오버라이딩

---
+++ src/main/java/MyMath2.java
@@ -0,0 +1,36 @@
+public class MyMath2 {
+    long a, b;
+
+    long add() {
+        return a + b;
+    }
+
+    long subtract() {
+        return a - b;
+    }
+
+    long multiply() {
+        return a * b;
+    }
+
+    double divide() {
+        return a / b;
+    }
+
+    static long add(long a, long b) {
+        return a + b;
+    }
+
+    static long subtract(long a, long b) {
+        return a - b;
+    }
+
+    static long multiply(long a, long b) {
+        return a * b;
+    }
+
+    static double divide(double a, double b) {
+        return a / b;
+    }
+
+}
