9811c1a54e85ee06b5027ab052be2d9140a72079
안경만 <rudaks012@naver.com>
2022-09-27 08:23:50 +0900

    Feat : 생성자 에서 다른생성자 호출하기

--- src/main/java/ch6/CarTest2.java
+++ src/main/java/ch6/CarTest2.java
@@ -1,6 +1,7 @@
 package ch6;
 
 class Car {
+
     String color;
     String gearType;
     int door;
@@ -12,16 +13,24 @@ class Car {
     Car(String color) {
         this(color, "auto", 4);
     }
+
     Car(String color, String gearType, int door) {
         this.color = color;
         this.gearType = gearType;
         this.door = door;
     }
-    Car(int number) {
-
-    }
 }
 
 public class CarTest2 {
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car("blue");
+        System.out.println(
+            "c2.color ,c2.door,c2.gearType = " + c2.color + "+" + c2.door + "+" + c2.gearType);
+        System.out.println(
+            "c1.color ,c1.door,c1.gearType = " + c1.color + "+" + c1.door + "+" + c1.gearType);
+    }
+
 
 }
