fb0dbc1abe19e180548fae0c183ff88ac2721471
안경만 <rudaks012@naver.com>
2022-09-27 08:34:14 +0900

    Feat : 생성자를 이용한 인스턴스 복 사

--- src/main/java/ch6/CarTest2.java
+++ src/main/java/ch6/CarTest2.java
@@ -1,25 +1,6 @@
 package ch6;
 
-class Car {
 
-    String color;
-    String gearType;
-    int door;
-
-    Car() {
-        this("white", "auto", 4);
-    }
-
-    Car(String color) {
-        this(color, "auto", 4);
-    }
-
-    Car(String color, String gearType, int door) {
-        this.color = color;
-        this.gearType = gearType;
-        this.door = door;
-    }
-}
 
 public class CarTest2 {
 
