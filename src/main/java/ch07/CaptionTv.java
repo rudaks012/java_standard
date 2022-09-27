cf51dff7c52ed69c7c64442c2c4b3f50afde5b79
gyeongman <rudaks012@naver.com>
2022-09-27 22:15:46 +0900

    Feat : 7단원 상속

---
+++ src/main/java/ch07/CaptionTv.java
@@ -0,0 +1,10 @@
+package ch07;
+
+public class CaptionTv extends Tv{
+    boolean caption;
+    void displayCaption(String text) {
+        if(caption) {
+            System.out.println(text);
+        }
+    }
+}
