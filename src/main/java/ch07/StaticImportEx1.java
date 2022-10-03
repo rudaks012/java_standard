ae9aac480138c7d5274eb5a0f9b6b99e705a8221
gyeongman <rudaks012@naver.com>
2022-10-03 17:20:38 +0900

    Feat : 7단원 : random 함수

---
+++ src/main/java/ch07/StaticImportEx1.java
@@ -0,0 +1,13 @@
+package ch07;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImportEx1 {
+    public static void main(String[] args) {
+//        System.out.println(Math.random());
+        out.println(random());
+
+        out.println("Math.PI :" + PI);
+    }
+}
