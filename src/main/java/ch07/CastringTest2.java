0d0fb97eb5f5729215c21ffa736d2910b4896dfe
gyeongman <rudaks012@naver.com>
2022-10-17 21:12:20 +0900

    Feat : 컴파일 에러 발생

---
+++ src/main/java/ch07/CastringTest2.java
@@ -0,0 +1,15 @@
+package ch07;
+
+public class CastringTest2 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Car car2 = null;
+        FireEngine fe = null;
+
+        car.drive();
+        fe = (FireEngine) car; // 컴파일은 된다 실행시 에러 발생
+        fe.drive();
+        car2 = fe;
+        car2.drive();
+    }
+}
