e410b388235670324af61b7c1ff4daaefe337d6c
gyeongman <rudaks012@naver.com>
2022-10-18 08:11:47 +0900

    Feat : 7단원 InstanceOf Test

--- src/main/java/ch07/InstanceofTest.java
+++ src/main/java/ch07/InstanceofTest.java
@@ -1,2 +1,24 @@
-package ch07;public class InstanceofTest {
+package ch07;
+
+public class InstanceofTest {
+    public static void main(String[] args) {
+        FireEngine1 fe = new FireEngine1();
+
+        if (fe instanceof FireEngine1) {
+            System.out.println("This is a FireEngine instance.");
+        }
+        if (fe instanceof Car1) {
+            System.out.println("This is a Car instance.");
+        }
+        if (fe instanceof Object) {
+            System.out.println("This is an Object instance");
+        }
+        System.out.println(fe.getClass().getName()); // 클래스의 이름을 출력
+    }
+}
+class Car1 {
+
+}
+class FireEngine1 extends Car1 {
+
 }
