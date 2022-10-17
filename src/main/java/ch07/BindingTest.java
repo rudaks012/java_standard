3665657c81e3952e68a02d4da107e1382d3cd3c6
gyeongman <rudaks012@naver.com>
2022-10-18 08:18:09 +0900

    Feat : 7단원 BindingTest (참조변수 확인)

---
+++ src/main/java/ch07/BindingTest.java
@@ -0,0 +1,35 @@
+package ch07;
+
+import java.sql.SQLOutput;
+
+public class BindingTest {
+    public static void main(String[] args) {
+        Parent1 p = new Child1();
+        Child1 c = new Child1();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+
+        System.out.println("c.s = " + c.x);
+        c.method();
+
+    }
+
+
+}
+
+class Parent1 {
+    int x = 100;
+
+    void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child1 extends Parent1 {
+    int x = 200;
+
+    void method() {
+        System.out.println("Child Method");
+    }
+}
--- src/main/java/ch07/SuperTest.java
+++ src/main/java/ch07/SuperTest.java
@@ -11,7 +11,7 @@ class Parent {
     int x = 10;
 }
 
-class Child extends Parent {
+class Child extends Parent1 {
     void method() {
         System.out.println("x=" + x);
         System.out.println("this.x=" + this.x);
