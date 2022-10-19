d9e23b482e657ea1462aace782625a03164b3e64
gyeongman <rudaks012@naver.com>
2022-10-19 21:35:07 +0900

    Feat : 7단원 인터페이스의 구현

--- src/main/java/ch07/Unit.java
+++ src/main/java/ch07/Unit.java
@@ -8,18 +8,18 @@ abstract class Unit {
     void stop() { /* 현재 위치에 정지 */ }
 }
 
-class Marine extends Unit {
+class Marine extends Unit1 {
     void move(int x, int y) { /* 지정된 위치로 이동 */ }
 
     void stimPack() { /* 스팀팩을 사용한다. */}
 }
 
-class Tank extends Unit {
+class Tank extends Unit1 {
     void move(int x, int y) { /* 지정된 위치로 이동 */ }
 
     void changeMode() { /* 공격모드를 변환한다. */}
 }
-class Dropship extends Unit {
+class Dropship extends Unit1 {
     void move(int x, int y) { /* 지정된 위치로 이동 */ }
 
     void load() { /* 선택된 대상을 태운다. */}
