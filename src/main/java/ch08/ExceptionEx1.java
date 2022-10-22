5651ade0a75dfc33ea4b6ab713f25559ff44edd3
gyeongman <rudaks012@naver.com>
2022-10-22 18:33:29 +0900

    Feat : 8단원 예외처리

---
+++ src/main/java/ch08/ExceptionEx1.java
@@ -0,0 +1,16 @@
+package ch08;
+
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+            try {
+            } catch (Exception e) {
+            }
+
+        } catch (Exception e) {
+//            try {
+//            } catch (Exception e) { //에러 변수 e가 중복 선언
+//            }
+        }
+    }
+}
