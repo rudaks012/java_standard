8ba0502bbf058a4273f602faeaf44c889feb9030
gyeongman <rudaks012@naver.com>
2022-12-01 00:19:19 +0900

    Feat : StringTokenize 사용법

--- src/main/java/ch09/StringTokenizerEx1.java
+++ src/main/java/ch09/StringTokenizerEx1.java
@@ -1,11 +1,5 @@
 package ch09;
 
-import java.util.StringTokenizer;
-
 public class StringTokenizerEx1 {
     public static void main(String[] args) {
-        String source = "100,200,300,400";
-        StringTokenizer st = new StringTokenizer(source, ",");
-        }
-    }
 }
